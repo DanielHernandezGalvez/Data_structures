8b257377ab284724cc1d9a177266289f2770e4ef
unknown <hernandezgalvezalejandro@gmail.com>
2024-07-18 15:54:34 -0600

    don´t use var

--- Intro/variable/main.js
+++ Intro/variable/main.js
@@ -4,3 +4,14 @@ console.log(animal)
 animal = 1
 
 console.log(animal)
+
+var namePerson = "Dinz"
+
+function print(n) {
+    let namePerson = n;
+    namePerson = "juan 2"
+    console.log(namePerson)
+}
+
+print("Kramer")
+console.log(namePerson)
