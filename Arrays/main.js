582f7cb5365351e175dc0b769b6ca0a59e80800c
unknown <hernandezgalvezalejandro@gmail.com>
2024-07-20 10:43:32 -0600

    arrays

---
+++ Arrays/main.js
@@ -0,0 +1,10 @@
+let array = [1, 2, "dinz", true];
+
+function showArray1(array) {
+  console.log("--------------");
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+showArray1(array);
