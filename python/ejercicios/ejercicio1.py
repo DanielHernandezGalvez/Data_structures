de82debf089f9bcdf853c8c0d041c3e146af7c8c
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-11 18:08:19 -0600

    anagrama

--- python/ejercicios/ejercicio1.py
+++ python/ejercicios/ejercicio1.py
@@ -6,5 +6,34 @@ por ejemplo: ("paris", "prisa")
 """
 
 def anagrama(str1, str2):
+    # si tiene espacios se los quitamos y convertimos a minusculas
     str1 = str1.replace(" ", "").lower()
     str2 = str2.replace(" ", "").lower()
+
+    if str1 == str1:
+        return False
+    
+    if len(str1) != len(str2):
+        return False
+    
+    contador = {}
+
+    for letra in str1:
+        if letra in contador:
+            contador[letra] += 1
+        else:
+            contador[letra] = 1
+
+    for letra in str2:
+        if letra in contador:
+            contador[letra] -= 1
+        else:
+            contador[letra] = 1
+
+    for elemento in contador:
+        if contador[elemento] != 0:
+            return False
+        
+    return True
+
+anagrama("paris", "prisa")
