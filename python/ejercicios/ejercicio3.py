7f9c4cd075c039473f20f61f7e336bf3af7539e9
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-18 12:43:06 -0600

    test a maximo_suma_continua()

--- python/ejercicios/ejercicio3.py
+++ python/ejercicios/ejercicio3.py
@@ -16,4 +16,19 @@ def maximo_suma_continua(lista):
     return maximo
 
  
-print( maximo_suma_continua([4,8,6,-3,2,-4,1,-1]))
+# print( maximo_suma_continua([4,8,6,-3,2,-4,1,-1]))
+
+# Clase de prueba de la funcionanterior "maximo_suma_continua"
+
+from nose.tools import assert_equal
+
+class Probas_maximo_suma_continua(object):
+    def prueba1(self):
+        assert_equal(maximo_suma_continua([4,8,6,-3,2,-4,1,-1]), 18)
+        assert_equal(maximo_suma_continua([1,5,-2,4,-3,2]), 8)
+        print("Todos las pruebas se han ejecutado correctamente")
+
+print( maximo_suma_continua([1,5,-2,4,-3,2]))
+
+prueba = Probas_maximo_suma_continua()
+prueba.prueba1()
