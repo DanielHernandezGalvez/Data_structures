5ea8e77ccf2c2eadfedc13cd6ccf3b67aa030ca4
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-18 13:01:47 -0600

    test de iguales

--- python/ejercicios/ejercicio4.py
+++ python/ejercicios/ejercicio4.py
@@ -16,9 +16,28 @@ def iguales(lista1, lista2):
     for num1, num2 in zip(lista1, lista2): # zip las hace de este modo (1,1), (1,2), (2,3)...
         if num1 != num2:
             return False
-        else:
-            return True
+       
+    return True
 
     # print(lista1, " ", lista2 )
 
-print(iguales([1,2,3,4,5,6], [6,5,4,3,2,2,1]))
+from nose.tools import assert_equal
+
+
+class Pruebas_iguales(object):
+    def test_iguales(self):
+        assert_equal(iguales([], []), True)
+        assert_equal(iguales([1], [1]), True)
+        assert_equal(iguales([2, 2, 2], [2, 2, 2]), True)
+
+        assert_equal(iguales([1, 2], [1, 2, 3]), False)
+        assert_equal(iguales([1, 2, 3], [1, 2, 4]), False)
+        assert_equal(iguales([1, 2, 3], [3, 2, 1]), True)
+        assert_equal(iguales([1, 2, 2], [1, 2]), False)
+
+        print("Todos los tests pasaron!")
+
+print(iguales([1,2,3,4,5,6], [6,5,4,3,2,1]))
+
+prueba = Pruebas_iguales()
+prueba.test_iguales()
