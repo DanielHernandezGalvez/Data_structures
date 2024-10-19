a44c8c2e00d4826a90edc61c813f57724361a8d7
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-19 10:46:40 -0600

    La clase Duplicados finciona con su método

--- python/ejercicios/ejercicio7.py
+++ python/ejercicios/ejercicio7.py
@@ -5,3 +5,20 @@ d = Duplicados()
 d.contiene_duplicados([1,2,3,4,1]) --> True
 d.contiene_duplicados([1,2,3,4]) --> False
 """
+
+class Duplicados:
+    def contiene_duplicados(self,numeros):
+        conjunto = set() 
+
+        for num in numeros:
+            if num in conjunto:
+                return True
+            else:
+                conjunto.add(num)
+            
+        return False
+    
+d = Duplicados()
+print(d.contiene_duplicados([1,2,3,4]))
+
+        
