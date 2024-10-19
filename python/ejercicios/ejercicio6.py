2edd4892d4b0ae4a2c594c7c850da455806d3dc5
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-19 10:27:43 -0600

    función comprimir lista

--- python/ejercicios/ejercicio6.py
+++ python/ejercicios/ejercicio6.py
@@ -3,3 +3,31 @@ Comprimir una cadena de letras en función de sus repeticiones
 comprimir("AAB") --> "A2B1"
 comprimir("AAAABBBCCdaaa") --> "A4B3C2D1a3"
 """
+
+def comprimir(cadena):
+
+    resultado = ""
+    longitud = len(cadena)
+
+    if longitud == 0:
+        return ""
+    
+    if longitud == 1:
+        return cadena + "1"
+    
+    contador = 1
+    i = 1
+
+    while i < longitud:
+        if cadena[i] == cadena[i - 1]:
+            contador += 1
+        else: 
+            resultado += cadena[i - 1] + str(contador)
+            contador = 1
+        i += 1
+    
+    resultado = resultado + cadena[i-1] + str(contador)
+
+    return resultado
+
+print(comprimir("AAAABBBCCdaaa"))
