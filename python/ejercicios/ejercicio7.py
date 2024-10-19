1b724d6c29744363eac3589a2f4c403e37fb23eb
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-19 10:40:37 -0600

    crear clase duplicados

---
+++ python/ejercicios/ejercicio7.py
@@ -0,0 +1,7 @@
+"""
+Construir una clase Duplicados y su función contiene_duplicados
+dado un array de numeros, devuelve True si hay duplicados, False en caso contrario
+d = Duplicados()
+d.contiene_duplicados([1,2,3,4,1]) --> True
+d.contiene_duplicados([1,2,3,4]) --> False
+"""
