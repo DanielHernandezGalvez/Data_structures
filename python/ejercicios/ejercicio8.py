b972b2f0ef6e6a848b96c458e5c33694590fbd29
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-19 11:05:21 -0600

    función dentro de una clase

---
+++ python/ejercicios/ejercicio8.py
@@ -0,0 +1,7 @@
+"""
+Funcion dentro de una clas
+inprime los indices de cada par de números que sumen el valor "suma" pasado como parámetro
+s.suma_2_elementos([2,3,4], 3) --> [0,1]
+s.suma_2_elementos([11,2,3,4,5,6,2], 4) --> [0,2]  [1,6]
+mejora: devolver una lista en lugar de imprimir el resultado
+"""
