9e7e5908b8f3fe680e9b4b44ef4121ae286d837b
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-20 11:07:57 -0600

    función dentro de una clase

--- python/ejercicios/ejercicio8.py
+++ python/ejercicios/ejercicio8.py
@@ -5,3 +5,17 @@ s.suma_2_elementos([2,3,4], 3) --> [0,1]
 s.suma_2_elementos([11,2,3,4,5,6,2], 4) --> [0,2]  [1,6]
 mejora: devolver una lista en lugar de imprimir el resultado
 """
+
+class Solucion:
+    def suma_2_elementos(self, numeros, suma):
+        diccionario = {} # valor: indice  1:0
+
+        for indice, numero in enumerate(numeros):
+            diferencia = suma - numero
+            if diferencia in diccionario:
+                print([diccionario[diferencia], indice])
+            diccionario[numero] = indice
+        return
+
+s = Solucion()
+s.suma_2_elementos([11,2,3,4,5,6,2], 4)
