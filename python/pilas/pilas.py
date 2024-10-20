2bffebe8510779ff4285911d9a948b88df1249f6
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-20 11:19:30 -0600

    lifo

---
+++ python/pilas/pilas.py
@@ -0,0 +1,36 @@
+"""
+Pilas
+LIFO: ultimo en entrar primero en salir
+ejemplo una pila de libros, el ultimo que pones es el primero que puedes quitar
+"""
+
+class Pila():
+    def __init__(self):
+        self.elementos = []
+    
+    def esta_vacia(self):
+        return self.elementos == []
+    
+    def insertar(self, elemento):
+        self.elementos.append(elemento)
+
+    def eliminar(self):
+        return self.elementos.pop()
+    
+    def consultar(self):
+        return self.elementos[len(self.elementos) - 1]
+    
+    def longitud(self):
+        return len(self.elementos)
+    
+p = Pila()
+p.insertar("libro verde")
+p.insertar("libro azul")
+p.insertar("libro rojo")
+
+
+p.longitud()
+
+print(p.longitud())
+print(p.eliminar())
+print(p.consultar())
