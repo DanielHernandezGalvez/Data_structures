106c83b23283810b3ef4d7f4f21452dddda89255
unknown <hernandezgalvezalejandro@gmail.com>
2024-10-27 11:14:13 -0600

    consigna de ejercicio de pilas 2

---
+++ python/pilas/ejercicios/ejercicio3.py
@@ -0,0 +1,13 @@
+"""
+Dada una lista de parentesis, corchetes y llaves pasados como parámetro
+a la función "balanceados", devolver True si los elementos estan balanceados o 
+False en caso contrario.
+Tipos de parentesis (), [], {}
+ejemplo balanceado ([]), (){}
+ejemplo no balanceado ([)]
+Pista: utilizar pilas para ir almacenando los elementos (parentesis, corchetes y llaves)
+balanceados ('[]') --> True
+balanceados ('[](){}') --> True
+balanceados ('[()]') --> True
+balanceados ('[](') --> True
+"""
