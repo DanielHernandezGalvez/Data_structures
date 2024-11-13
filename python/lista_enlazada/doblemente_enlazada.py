fe03e1e24ce96c564fd7f315bb664d73edbf24cd
unknown <hernandezgalvezalejandro@gmail.com>
2024-11-13 12:52:50 -0600

    lista doblemente enlazada

--- python/lista_enlazada/doblemente_enlazada.py
+++ python/lista_enlazada/doblemente_enlazada.py
@@ -1,2 +1,19 @@
 """
 """
+
+class Nodo():
+    def __init__(self, valor):
+        self.valor = valor
+        self.nodo_siguiente = None
+        self.nodo_anterior = None
+
+nodo1 = Nodo("valor 1")
+nodo2 = Nodo("valor 2")
+nodo3 = Nodo("valor 3")
+
+nodo1.nodo_siguiente = nodo2
+nodo2.nodo_anterior= nodo1
+nodo2.nodo_siguiente = nodo3
+nodo3.nodo_anterior = nodo2
+
+print(nodo2.nodo_siguiente.valor)
