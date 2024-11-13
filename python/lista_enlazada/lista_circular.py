1fb515c8d58ab3d8b27c1971a69bc3a60173bbbe
unknown <hernandezgalvezalejandro@gmail.com>
2024-11-13 12:59:26 -0600

    lista circular

--- python/lista_enlazada/lista_circular.py
+++ python/lista_enlazada/lista_circular.py
@@ -8,9 +8,17 @@ nodo1 = Nodo("valor 1")
 nodo2 = Nodo("valor 2")
 nodo3 = Nodo("valor 3")
 
+# enlace 1 con 3
+
 nodo1.nodo_siguiente = nodo2
-nodo2.nodo_anterior= nodo1
+nodo2.nodo_anterior = nodo1
+
+# enlace 2 con 3
 nodo2.nodo_siguiente = nodo3
 nodo3.nodo_anterior = nodo2
 
-print(nodo3.nodo_anterior.valor)
+# enlace 1 con 3
+nodo3.nodo_siguiente = nodo1
+nodo1.nodo_anterior = nodo3
+
+print(nodo1.nodo_anterior.valor)
