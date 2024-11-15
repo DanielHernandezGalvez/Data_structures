9fe8658d2ff30857c4ad41946d6b4ca6505cfb8b
unknown <hernandezgalvezalejandro@gmail.com>
2024-11-15 11:09:05 -0600

    test

--- python/lista_enlazada/ejercicios/ejercicio1.py
+++ python/lista_enlazada/ejercicios/ejercicio1.py
@@ -40,3 +40,15 @@ nodo6 = Nodo(6)
 nodo5.nodo_siguiente = nodo6
 
 print(circular(nodo5))
+
+from nose.tools import assert_equal
+
+class Pruebas():
+    def test_circular(self):
+        assert_equal(circular(nodo6), False)
+        assert_equal(circular(nodo1), True)
+        print("Exito al ejecutar pruebas")
+
+p = Pruebas()
+p.test_circular()
+
