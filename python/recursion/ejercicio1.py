ced0f09a1199fbbed2b1aa817bf531589a4800d7
unknown <hernandezgalvezalejandro@gmail.com>
2024-11-15 11:39:32 -0600

    test en recursion

--- python/recursion/ejercicio1.py
+++ python/recursion/ejercicio1.py
@@ -12,3 +12,14 @@ def reves(cadena):
         return reves(cadena[1:]) + cadena[0]
     
 print(reves("123"))
+
+from nose.tools import assert_equal
+class TestReves(object):
+    def test_reves(self):
+        assert_equal(reves("abc"), "cba")
+        assert_equal(reves("hola"), "aloh")
+        assert_equal(reves("123"), "321")
+        print('test pasados correctamente')
+
+p = TestReves()
+p.test_reves()  # run the tests
