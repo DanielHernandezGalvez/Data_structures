1f86a21e656ab7382600163ab233b4e4cd059a84
unknown <hernandezgalvezalejandro@gmail.com>
2024-11-18 13:04:38 -0600

    grafos dirigidos

--- python/grafos/grafos.py
+++ python/grafos/grafos.py
@@ -1,61 +1,66 @@
-class Vertice():
+class Vertice:
     def __init__(self, clave):
         self.id = clave
         self.conexiones = {}
-        
+
     def incluir_vecino(self, vecino, peso):
         self.conexiones[vecino] = peso
-        
+
     def ver_conexiones(self):
         return self.conexiones.keys()
-    
+
     def ver_id(self):
         return self.id
-    
+
     def ver_peso(self, vecino):
         return self.conexiones[vecino]
-    
+
     def __str__(self):
-        # print(vertice) 
-        return str(self.id) + " conectado con " + str([ x for x in self.ver_conexiones()])
-        
-        
-vertice = Vertice(5)
-vertice.ver_id()
+        return str(self.id) + " conectado con " + str([x for x in self.ver_conexiones()])
 
 
-class Grafo():
+class Grafo:
     def __init__(self):
         self.vertices = {}
         self.num_vertices = 0
-        
+
     def incluir_vertice(self, clave):
         nuevo_vertice = Vertice(clave)
         self.vertices[clave] = nuevo_vertice
-        self.num_vertices += 2
+        self.num_vertices += 1
         return nuevo_vertice
-    
+
     def ver_vertice(self, clave):
-        if clave in self.vertices:
-            return self.vertices[clave]
-        else:
-            return None
-        
-    def incluir_arista(self, origen,destino,peso=0):
+        return self.vertices.get(clave, None)
+
+    def incluir_arista(self, origen, destino, peso=0):
         if origen not in self.vertices:
             self.incluir_vertice(origen)
-        if destino not in self.ver_vertices:
+        if destino not in self.vertices:
             self.incluir_vertice(destino)
-        
-        self.ver_vertice[origen].incluir_vecino(destino, peso)
-        
+
+        self.ver_vertice(origen).incluir_vecino(destino, peso)
+
     def ver_lista_vertices(self):
         return self.vertices.keys()
-    
+
     def __iter__(self):
         return iter(self.vertices.values())
-        
-        
-        
+
+
+# Uso del grafo
 grafo = Grafo()
-        
+grafo.incluir_vertice(1)
+grafo.incluir_vertice(2)
+grafo.incluir_vertice(3)
+grafo.incluir_vertice(4)
+
+grafo.incluir_arista(1, 2, 4)
+grafo.incluir_arista(1, 4, 2)
+grafo.incluir_arista(2, 4, 1)
+grafo.incluir_arista(2, 3, 1)
+
+print("Vértices en el grafo:", grafo.ver_lista_vertices())
+
+for vertice in grafo:
+    print(vertice)
